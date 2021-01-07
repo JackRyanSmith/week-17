bc7a0f6a5dfebd39d98647856e0b2977ffa526e0
JackRyan_Smith <JackRyan3d@gmail.com>
2021-01-06 19:51:34 -0800

    hw

--- server.js
+++ server.js
@@ -1,49 +1,35 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-const path = require("path");
-
-const PORT = process.env.PORT || 3003;
-
-const db = require("./models");
+const compression = require("compression");
 
 const app = express();
 
-app.use(logger("dev"));
-app.use(express.static("public"));
+const apiRoutes = require("./routes/api");
+const indexRoutes = require("./routes/index");
 
+const PORT = process.env.PORT || 3000;
+
+app.use(logger("dev"));
+
+app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+// process.env.MONGODBURL = "mongodb://localhost:27017/workout";
 
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+});
 
-// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout_trackerdb',
-//  { useNewUrlParser: true, useUnifiedTopology: true }).catch(error => handleError(error));
+// routes
+app.use("/", indexRoutes);
+app.use("/api", apiRoutes);
 
-//or
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout_trackerdb',
- { useNewUrlParser: true, useUnifiedTopology: true });
- mongoose.connection
-  .once('open', () => console.log('Good to go!'))
-  .on('error', (error) => {
-      console.warn('Error', error);
-  });
-
-db.Workouts.create({name: "Get 'Er Done Workout Tracker"})
-  .then(dbWorkouts => {
-    console.log('log from inside dbWorkouts.create '+dbWorkouts);
-  })
-  .catch( ({message}) => {
-    console.log('this is the error:' +message);
-  });
-
-  app.get("/", (req,res) =>{
-    console.log('GET root route');
-    res.sendFile(path.join(__dirname, "./public/index.html"));
-  })
-  app.use(require("./controllers/exercises"));
-  app.use(require("./controllers/workout"));
-  
-  app.listen(PORT, () => {
-    console.log(`App running on port ${PORT}!`);
-  });
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
